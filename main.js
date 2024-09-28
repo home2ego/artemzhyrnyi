62a4980281393bd26b9894fda87d94043f39b414
Artem Zhyrnyi <ic3ego@gmail.com>
2024-09-28 19:48:36 +0200

    fix toggle theme button

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-// set the light-heme if the user's operating system or browser has the light-theme by default
+const toggleTheme = document.querySelector('#toggle-theme');
+const themeIcon = document.querySelector('#theme-icon');
 
+// Function to update the icon and aria-label
+function updateThemeIcon() {
+  const isLightTheme = document.documentElement.classList.contains('light-theme');
+
+  themeIcon.setAttribute('src', isLightTheme ? '/assets/moon.svg' : '/assets/sun.svg');
+  toggleTheme.setAttribute('aria-label', isLightTheme ? 'Toggle to dark mode' : 'Toggle to light mode');
+}
+
+// Set theme based on the default OS preferences
 const prefersLight = window.matchMedia('(prefers-color-scheme: light)').matches;
 
 if (prefersLight) {
-  document.documentElement.classList.add('light-theme');
+  document.documentElement.classList.add('light-theme'); // Apply system light-theme
+
+  // Update the icon and aria-label
+  updateThemeIcon();
 }
 
-// toggle the light-theme
-
-document.querySelector('#toggle-theme').addEventListener('click', () => {
+// Toggle theme and save preference on click
+toggleTheme.addEventListener('click', () => {
   document.documentElement.classList.toggle('light-theme');
+
+  // Update the icon and aria-label
+  updateThemeIcon();
 });
