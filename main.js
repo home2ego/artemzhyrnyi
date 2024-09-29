df59556e79944b1d54419dd288eb52609f086f9e
Artem Zhyrnyi <ic3ego@gmail.com>
2024-09-29 21:03:09 +0200

    fix theme icons

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const toggleTheme = document.querySelector('#toggle-theme');
-const themeIcon = document.querySelector('#theme-icon');
 
-// Function to update the icon and aria-label
-function updateThemeIcon() {
+// Function to update aria-label
+function updateLabel() {
   const isLightTheme = document.documentElement.classList.contains('light-theme');
 
-  themeIcon.setAttribute('src', isLightTheme ? '/moon.svg' : '/sun.svg');
   toggleTheme.setAttribute('aria-label', isLightTheme ? 'Toggle to dark mode' : 'Toggle to light mode');
 }
 
@@ -15,14 +13,14 @@ const prefersLight = window.matchMedia('(prefers-color-scheme: light)').matches;
 if (prefersLight) {
   document.documentElement.classList.add('light-theme'); // Apply system light-theme
 
-  // Update the icon and aria-label
-  updateThemeIcon();
+  // Update aria-label
+  updateLabel();
 }
 
 // Toggle theme and save preference on click
 toggleTheme.addEventListener('click', () => {
   document.documentElement.classList.toggle('light-theme');
 
-  // Update the icon and aria-label
-  updateThemeIcon();
+  // Update aria-label
+  updateLabel();
 });
