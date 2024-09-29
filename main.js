32815d006b2d8775b84d492d6cf1913a1930c014
Artem Zhyrnyi <ic3ego@gmail.com>
2024-09-29 21:43:44 +0200

    add correct light theme

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 const toggleTheme = document.querySelector('#toggle-theme');
 
-// Function to update aria-label
-function updateLabel() {
-  const isLightTheme = document.documentElement.classList.contains('light-theme');
-
-  toggleTheme.setAttribute('aria-label', isLightTheme ? 'Toggle to dark mode' : 'Toggle to light mode');
-}
-
 // Set theme based on the default OS preferences
 const prefersLight = window.matchMedia('(prefers-color-scheme: light)').matches;
 
@@ -14,7 +7,7 @@ if (prefersLight) {
   document.documentElement.classList.add('light-theme'); // Apply system light-theme
 
   // Update aria-label
-  updateLabel();
+  toggleTheme.setAttribute('aria-label', 'Toggle to dark mode');
 }
 
 // Toggle theme and save preference on click
@@ -22,5 +15,7 @@ toggleTheme.addEventListener('click', () => {
   document.documentElement.classList.toggle('light-theme');
 
   // Update aria-label
-  updateLabel();
+  const isLightTheme = document.documentElement.classList.contains('light-theme');
+
+  toggleTheme.setAttribute('aria-label', isLightTheme ? 'Toggle to dark mode' : 'Toggle to light mode');
 });
