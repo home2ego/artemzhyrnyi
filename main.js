23638244432515babf202ec06fb58cf0e8778e7f
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-16 13:30:37 +0200

    fix local storage

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 const toggleTheme = document.querySelector("#toggle-theme");
 
 // Check if there's a saved light theme in localStorage
-const savedTheme = localStorage.getItem("isLightTheme");
+const savedLightTheme = localStorage.getItem("isLightTheme") === true;
 
 // Set theme based on the default OS preferences
 const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
 
-if (prefersLight) {
+if (savedLightTheme || prefersLight) {
     document.documentElement.classList.add("light-theme"); // Apply system light-theme
 
     // Update aria-label
