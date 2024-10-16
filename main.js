0435494e259a5913269ef07ae7d5b180211a4012
Artem Zhyrnyi <ic3ego@gmail.com>
2024-10-16 13:37:21 +0200

    fix local storage #2

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
 const toggleTheme = document.querySelector("#toggle-theme");
 
-// Check if there's a saved light theme in localStorage
-const savedLightTheme = localStorage.getItem("isLightTheme") === true;
-
-// Set theme based on the default OS preferences
-const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
-
-if (savedLightTheme || prefersLight) {
-    document.documentElement.classList.add("light-theme"); // Apply system light-theme
-
-    // Update aria-label
-    toggleTheme.setAttribute("aria-label", "Toggle to dark mode");
-}
-
 // Toggle theme and save preference on click
 toggleTheme.addEventListener("click", () => {
     document.documentElement.classList.toggle("light-theme");
