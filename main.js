babe55462449a8e99a9a607524f05b44e9bd2bec
Artem Zhyrnyi <ic3ego@gmail.com>
2025-01-31 22:47:06 +0200

    refactoring code above the fold

--- main.js
+++ main.js
@@ -1,30 +1,21 @@
 const toggleTheme = document.querySelector("#toggle-theme");
 
-/* Set theme based on saved preference or default OS preference */
+// Set theme based on saved browser settings or OS default settings
 const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
 
 if (prefersLight) {
-    /* Apply light theme before page loads */
+    // Apply light theme before page loads
     document.documentElement.classList.add("light-theme");
 }
 
-// Toggle theme and save preference on click
-toggleTheme.addEventListener("click", () => {
-    document.documentElement.classList.toggle("light-theme");
-
-    const isLightTheme =
-        document.documentElement.classList.contains("light-theme");
-
-    // Update aria-label
-    toggleTheme.setAttribute(
-        "aria-label",
-        isLightTheme ? "Toggle to dark mode" : "Toggle to light mode"
-    );
-});
+// Toggle theme
+toggleTheme.addEventListener("click", () =>
+    document.documentElement.classList.toggle("light-theme")
+);
 
 // Scroll down arrow
 document.querySelector("#scroll-down").addEventListener("click", () => {
     window.scrollTo({
-        top: document.querySelector("#projects").offsetTop - 45,
+        top: document.querySelector("#about").offsetTop - 95,
     });
 });
