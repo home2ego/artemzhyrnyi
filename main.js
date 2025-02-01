3cf338dcce662db689b9fb19003bed136c4848c1
Artem Zhyrnyi <ic3ego@gmail.com>
2025-02-01 20:37:27 +0200

    code refactoring

--- main.js
+++ main.js
@@ -4,18 +4,18 @@ const toggleTheme = document.querySelector("#toggle-theme");
 const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
 
 if (prefersLight) {
-    // Apply light theme before page loads
-    document.documentElement.classList.add("light-theme");
+  // Apply light theme before page loads
+  document.documentElement.classList.add("light-theme");
 }
 
 // Toggle theme
 toggleTheme.addEventListener("click", () =>
-    document.documentElement.classList.toggle("light-theme")
+  document.documentElement.classList.toggle("light-theme")
 );
 
 // Scroll down arrow
 document.querySelector("#scroll-down").addEventListener("click", () => {
-    window.scrollTo({
-        top: document.querySelector("#about").offsetTop - 95,
-    });
+  window.scrollTo({
+    top: document.querySelector("#about").offsetTop - 95,
+  });
 });
