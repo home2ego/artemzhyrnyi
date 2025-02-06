4157a3df9d98e6352e28dbee4fdc5dc646099e31
Artem Zhyrnyi <ic3ego@gmail.com>
2025-02-06 10:29:08 +0200

    add menu

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const toggleTheme = document.querySelector("#toggle-theme");
 const toggleMenu = document.querySelector("#toggle-menu");
+const logo = document.querySelector("#logo");
+const menuButtons = document.querySelectorAll("#menu .btn");
+
+const isValidWindowSize = window.matchMedia("(max-width: 46.5rem)");
+
+function setAreaExpanded(bool) {
+  return toggleMenu.setAttribute("aria-expanded", bool);
+}
 
 // Set theme based on saved browser settings or OS default settings
 const prefersLight = window.matchMedia("(prefers-color-scheme: light)").matches;
@@ -14,14 +22,31 @@ toggleTheme.addEventListener("click", () =>
   document.documentElement.classList.toggle("light-theme")
 );
 
+// Toggle menu
+toggleMenu.addEventListener("click", () => {
+  const isExpanded = toggleMenu.getAttribute("aria-expanded") === "true";
+
+  setAreaExpanded(!isExpanded);
+});
+
+// Close menu by clicking the button
+logo.addEventListener(
+  "click",
+  () => isValidWindowSize.matches && setAreaExpanded(false)
+);
+
+for (const btn of menuButtons) {
+  btn.addEventListener(
+    "click",
+    () => isValidWindowSize.matches && setAreaExpanded(false)
+  );
+}
+
 // Scroll down arrow
 document.querySelector("#scroll-down").addEventListener("click", () => {
   window.scrollTo({
     top: document.querySelector("#about").offsetTop - 100,
   });
-});
 
-// Toggle menu
-toggleMenu.addEventListener("click", () =>
-  toggleMenu.classList.toggle("is-open")
-);
+  return isValidWindowSize.matches && setAreaExpanded(false);
+});
